4d22fccb609ae2d1867ffa060ca300664e0458a6
Diener Dornelas <diener.ld@outlook.com>
2022-06-01 17:43:26 -0300

    update openapi

--- src/main/java/br/edu/ufsj/tecweb/pigman/Repository/StallRepository.java
+++ src/main/java/br/edu/ufsj/tecweb/pigman/Repository/StallRepository.java
@@ -1,10 +1,6 @@
 package br.edu.ufsj.tecweb.pigman.Repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import br.edu.ufsj.tecweb.pigman.Domain.Stall;
 
